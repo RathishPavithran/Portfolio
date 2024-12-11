764ade1203d7d9ea902fddad5ffe8a905e0a9918
Rathish Pavithran <rathish1227@gmail.com>
2024-12-11 18:41:22 +0530

    src

--- src/components/About/Certificate.jsx
+++ src/components/About/Certificate.jsx
@@ -28,8 +28,7 @@ function Certificate() {
               location="Tirunelveli"
               timeline="September 2024 - November 2024"
               description="I completed a comprehensive front-end development course where I gained in-depth knowledge of HTML, CSS, and JavaScript. The program emphasized responsive web design, teaching me how to create mobile-friendly, cross-browser compatible websites. I worked extensively with modern tools and frameworks like Bootstrap and Tailwind CSS, building interactive user interfaces that prioritize user experience."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              // demoLink="https://chatify-49.web.app/"
+              // demoLink="#"
             />
           </Col>
           <Col md={4} className="certificate-card">
@@ -41,8 +40,7 @@ function Certificate() {
               location="Thoothukudi"
               timeline="December 2024 - Ongoing"
               description="I am undergoing intensive training in a back-end development course, where I focus on the MERN stack (MongoDB, Express, React, Node.js). This course provides me with hands-on experience in building scalable, full-stack applications. I gain expertise in creating server-side logic, managing databases with MongoDB, and developing RESTful APIs using Express."
-              // ghLink="https://github.com/soumyajit4419/Chatify"
-              // demoLink="https://chatify-49.web.app/"
+              // demoLink="#"
             />
           </Col>
         </Row>
